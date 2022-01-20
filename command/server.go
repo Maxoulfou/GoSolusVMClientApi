8c293ee60efdc9c855c5cf58eb3219c6e27ae9fb
Maxoulfou <hFEquTKmU9gr%/%>
2022-01-20 11:25:19 +0100

    Add server output format functions

---
+++ command/server.go
@@ -0,0 +1,23 @@
+package command
+
+import "fmt"
+
+func Reboot(array []string) {
+	fmt.Printf("Status: %+v\nStatus MSG: %+v\nHostname: %+v\nIP: %+v\n", array[0], array[1], array[2], array[3])
+}
+
+func Boot(array []string) {
+	fmt.Printf("Status: %+v\nStatus MSG: %+v\nHostname: %+v\nIP: %+v\n", array[0], array[1], array[2], array[3])
+}
+
+func Shutdown(array []string) {
+	fmt.Printf("Status: %+v\nStatus MSG: %+v\nHostname: %+v\nIP: %+v\n", array[0], array[1], array[2], array[3])
+}
+
+func Status(array []string) {
+	fmt.Printf("Status: %+v\nStatus MSG: %+v\nVM Stat: %+v\nHostname: %+v\nIP: %+v\n", array[0], array[1], array[2], array[3], array[4])
+}
+
+func Info(array []string) {
+	fmt.Printf("Status: %+v\nHostname: %+v\nIP: %+v\n", array[0], array[2], array[3])
+}
